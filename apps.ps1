ce14041aa5271b0d0f3b8c6ea6841bd8df66f216
Torchikaii <72319744+Torchikaii@users.noreply.github.com>
2024-02-02 20:23:52 +0200

    added files

---
+++ apps.ps1
@@ -0,0 +1,27 @@
+
+
+# main apps
+choco install brave -y
+choco install 7zip -y
+choco install vlc -y
+choco install Everything -y
+choco install lightshot -y
+
+#choco install Office365ProPlus -y
+#choco install office2019proplus -y # – possibly broken
+
+# univ
+choco install forticlientvpn -y
+choco install ltspice -y
+
+# pc
+choco install hwinfo -y
+choco install ntlite-free -y
+choco install virtualbox -y
+
+# dev
+choco install git -y
+choco install pulsar -y
+choco install PyCharm-community -y
+
+pause
