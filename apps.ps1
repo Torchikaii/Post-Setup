995aa2ab33efd00d9bcd6e89843eddcaad97a63e
unknown <72319744+Torchikaii@users.noreply.github.com>
2024-10-18 23:02:08 +0300

    added recuva to apps.ps1, disabled forticlientvpn

--- apps.ps1
+++ apps.ps1
@@ -6,19 +6,21 @@ choco install 7zip -y
 choco install vlc -y
 choco install Everything -y
 choco install lightshot -y
+choco install revo-uninstaller -y
+
 
 #choco install Office365ProPlus -y
 #choco install office2019proplus -y # – possibly broken
 
 # univ
-choco install forticlientvpn -y
+#choco install forticlientvpn -y
 choco install ltspice -y
 
 # pc
 choco install hwinfo -y
 choco install ntlite-free -y
 choco install virtualbox -y
-choco install revo-uninstaller -y
+choco install Recuva -y
 
 # dev
 choco install git -y
