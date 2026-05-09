0c4e1aefa2a608420de9a504e41b398fa0b11ce8
Torchikaii <72319744+Torchikaii@users.noreply.github.com>
2026-05-09 15:08:07 +0300

    simplify apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -14,18 +14,18 @@ choco install revo-uninstaller -y
 
 # univ
 #choco install forticlientvpn -y
-choco install ltspice -y
+#choco install ltspice -y
 
 # pc
 choco install hwinfo -y
-choco install ntlite-free -y
-choco install virtualbox -y
-choco install Recuva -y
+#choco install ntlite-free -y
+#choco install virtualbox -y
+#choco install Recuva -y
 
 # dev
-choco install javaruntime -y
-choco install git -y
-choco install pulsar -y
+#choco install javaruntime -y
+#choco install git -y
+#choco install pulsar -y
 #choco install PyCharm-community -y
 
 # productivity
